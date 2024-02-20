688281c573988984da0fd5718d5cae9968f16f12
Dissiyt <dissi.yt221@gmail.com>
2024-02-20 13:39:13 +0100

    Add body-parese in package.json fix reservation.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 
-import path from 'path'
 export default defineNuxtConfig({
+
   devtools: { enabled: true },
   devServer: {
     port: 8080,
--- server/api/reservation.ts
+++ server/api/reservation.ts
@@ -1,29 +1,15 @@
 import type { Reservation } from "~/types";
 // @ts-ignore
-export default defineEventHandler(async (req, res) => {
+export default defineEventHandler(async (event) => {
     // Make the fetch request to the reservation server
-    if (req.method === "POST") {
-        try {
-            // No need to parse the request body manually. Express.js does it for you.
-            const setReservation = (data?: any) => (req._requestBody = data)
-            const reserve = async (data: any) => {
-                try {
+    const body = await readBody(event);
                     const res = await $fetch<Reservation>("http://ictbelplawp01:8080/reservations", {
                         method: "POST",
-                        body: data,
+                        headers: {
+                            'Content-Type': 'application/json'
+                        },
+                        body: body,
                     })
-                    setReservation(res.reservant)
                     console.log(res)
-                } catch (error) {
-                    setReservation()
-                    console.log(error)
-                }
-            }
-            // Accessing deskId and reservant from the request body
-            const { deskId, reservant } = req.body;
-            await reserve({ deskId, reservant });
-        } catch (error) {
-            console.log(error)
-        }
-    }
+
 });
